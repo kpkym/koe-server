3d056f8af9eeadbb9ef4c7ab4b781dd81a3a69b5
kpkym <okpkym@gmail.com>
2022-07-08 10:19:40 +0800

    get_file

--- src/file/nas.rs
+++ src/file/nas.rs
@@ -1,4 +1,19 @@
+use std::ffi::OsStr;
+use std::fs;
+
 pub fn get_file() -> String {
-    
-    "".to_string()
+    let dir = super::common::expand_tilde("~/config/koe/nas_koe_data".to_string());
+    let paths = fs::read_dir(dir).unwrap();
+    let mut result = vec![];
+
+
+    for path in paths {
+        let path = path.unwrap().path();
+        if let Some("json") = path.extension().and_then(OsStr::to_str) {
+            result.push(path);
+        }
+    }
+
+    result.sort();
+    result.last().unwrap().to_str().unwrap().to_string()
 }
